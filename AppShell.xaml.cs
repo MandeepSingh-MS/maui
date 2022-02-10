1779e4f06fc2ce9c8d3e40458ef2d856700171e9
Mandeep Singh <mandeepsingh2893@gmail.com>
2022-02-10 18:15:23 +0530

    changes

---
+++ AppShell.xaml.cs
@@ -0,0 +1,12 @@
+namespace MAUI;
+
+public partial class AppShell : Shell
+{
+
+
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
+
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,18 +9,13 @@ public partial class MainPage : ContentPage
         InitializeComponent();
     }
 
-    async private void OnCounterClicked2(object sender, EventArgs e)
-    {
-        await DisplayAlert("Alert", "You have been alerted", "OK");
-    }
+
 
 
     private void OnCounterClicked(object sender, EventArgs e)
     {
         count++;
         CounterLabel.Text = $"Current count: {count}";
-
-        SemanticScreenReader.Announce(CounterLabel.Text);
     }
 }
 
